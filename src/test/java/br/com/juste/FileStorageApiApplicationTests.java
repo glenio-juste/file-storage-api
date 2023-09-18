c09ac885b21ea4a46034a4880876da82350817f6
glenio-juste <glenio.juste@gmail.com>
2023-09-18 18:54:51 -0300

    Commit inicial do projeto

---
+++ src/test/java/br/com/juste/FileStorageApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.juste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileStorageApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
